43dd3affe01d8493e791a1ca4b263a31e0c6ae11
rohit <rohitchaudhary634@gmail.com>
2026-04-12 12:49:32 +0530

    api url changes

--- src/main/java/com/finder/letscheck/security/SecurityConfig.java
+++ src/main/java/com/finder/letscheck/security/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
+                                "/api/**",
                                 "/auth/**",
                                 "/search/**",
                                 "/items/**",
